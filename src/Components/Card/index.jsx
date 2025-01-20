427d70a4281547a48c3e6f48c23b819286e53906
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2025-01-20 18:21:52 -0300

    Consumiendo la FakeStore API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,17 +1,17 @@
-const Card = () => {
+const Card = ({data}) => {
     return (
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-2 left-2 bg-white/60 rounded-lg text-xs px-3 py-0.5'>Electronics</span>
+                <span className='absolute bottom-2 left-2 bg-white/60 rounded-lg text-xs px-3 py-0.5'>{data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg'
-                src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='Headphones'></img>
+                src={data.images[0]} alt={data.title}></img>
                 <div className='absolute top-2 right-2 flex justify-center items-center bg-white w-6 h-6 rounded-full p-1'>
                     +
                 </div>
             </figure>
             <p className='flex justify-between items-center'>
-                <span className='text-sm font-light'>Headphones</span>
-                <span className='text-lg font-medium'>$300</span>
+                <span className='text-sm font-light'>{data.title}</span>
+                <span className='text-lg font-medium'>${data.price}</span>
             </p>
         </div>
     )
